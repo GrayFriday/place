996dc08260a15d2a65dcf59ee9e56df492dd7a25
1298601551@qq.com <1298601551@qq.com>
2019-07-05 15:02:37 +0800

    75

--- pages/preview/preview.js
+++ pages/preview/preview.js
@@ -10,42 +10,42 @@ Page({
    */
 
   data: {
-    id:'',
+    id: '',
     scale: 14,
-    markers1: [],
+    markers1: [{
+      callout: {}
+    }],
     latitude: '',
     longitude: '',
     textData: {},
     polyline: [{
-      points: [
-      ],
+      points: [],
       color: "#FF0000",
       width: 3,
       dottedLine: false
     }],
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function ( options ) {
+  onLoad: function(options) {
     wx.showLoading({
       title: '加载中',
     })
-    console.log( options )
     this.data.id = options.id;
     var that = this;
     var myAmapFun = new amapFile.AMapWX({
       key: '06127e36a780db3eb68addd63fa1be36'
     });
     myAmapFun.getRegeo({
-      success: function (data) {
+      success: function(data) {
         that.getStadium()
 
       },
-      fail: function (info) {
+      fail: function(info) {
         //失败回调
-        console.log(info)
       }
     })
 
@@ -58,7 +58,7 @@ Page({
 
   },
   // 地图放大
-  fangDaTap: function (e) {
+  fangDaTap: function(e) {
     var that = this;
     console.log("scale===" + this.data.scale);
     that.setData({
@@ -67,8 +67,8 @@ Page({
   },
 
   // 地图缩小
-  
-  suoXiaoTap: function (e) {
+
+  suoXiaoTap: function(e) {
     var that = this;
     console.log("scale===" + this.data.scale);
     that.setData({
@@ -76,9 +76,9 @@ Page({
     })
   },
 
-  getStadium: function () {
+  getStadium: function() {
     const data = {
-      id:this.data.id
+      id: this.data.id
     }
     const url = '/api/v1/user/livePreview'
     http(url, data, "POST").then(res => {
@@ -88,10 +88,8 @@ Page({
         var dataPlace = res.data.Data
         const markers = []
         const points = []
+        var datas = {}
         for (let index in dataPlace) {
-          if (!markers[index]) {
-            markers[index] = []
-          }
           let longitude = dataPlace[index].longitude
           let latitude = dataPlace[index].latitude
           let title = dataPlace[index].title
@@ -100,14 +98,18 @@ Page({
             longitude,
             latitude
           }
-          var datas = {
+          datas = {
             longitude,
             latitude,
             iconPath: "../../images/danghui.png",
             id,
             width: 30,
             height: 30,
-            title
+            callout: {
+              content: title,
+              color: '#000000',
+              display: 'ALWAYS'
+            }
           }
           markers.push(datas)
           points.push(pointsData)
@@ -115,14 +117,14 @@ Page({
         this.data.polyline[0].points = points
         that.setData({
           markers1: markers,
-          polyline: this.data.polyline
+          polyline: this.data.polyline,
         })
         wx.hideLoading()
       }
     })
   },
 
-  onShow() { },
+  onShow() {},
   // 微信获得经纬度
   getLocation: () => {
     return new Promise((resolve, reject) => {
@@ -131,39 +133,11 @@ Page({
         success: (res) => {
           resolve(res)
         },
-        fail: function (res) {
+        fail: function(res) {
           reject('fail' + JSON.stringify(res))
         }
       })
     })
   },
-
-  /**
-   * 将焦点给到 input（在真机上不能获取input焦点）
-   */
-  tapInput() {
-    this.setData({
-      //在真机上将焦点给input
-      inputFocus: true,
-      //初始占位清空
-      inputInfo: ''
-    });
-  },
-
-  /**
-   * input 失去焦点后将 input 的输入内容给到cover-view
-   */
-  blurInput(e) {
-    this.setData({
-      inputInfo: e.detail.value || '输入'
-    });
-  },
-
-  toIndex: function () {
-    this.setData({
-      showModal_0: false
-    })
-  },
-
 
 })
